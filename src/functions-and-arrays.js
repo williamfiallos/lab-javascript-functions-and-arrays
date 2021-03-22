6dee562ec3372d1bbf688e9e1e95eed4abc5199c
William Fiallos <wfiallos10@gmail.com>
2021-03-22 19:14:14 -0400

    iteration 3 calculate the sum complete

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -26,8 +26,16 @@ function findLongestWord(words) {
 };
 
 // Iteration #3: Calculate the sum
-
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
+function sumNumbers(numbers) {
+  let sum = 0;
+  if (!numbers.length) return 0;
+  
+  for (let i = 0; i < numbers.length; i++) {
+    sum += numbers[i];
+  }
+  return sum;
+};
 
 // Iteration #4: Calculate the average
 // Level 1: Array of numbers
